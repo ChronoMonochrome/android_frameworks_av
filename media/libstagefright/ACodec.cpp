ae7beb35b19f116c4c6b2c354229752b35e249a0
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-16 03:25:04 +0700

    frameworks-av_001-AV-fix.patch
    Change-Id: I95162f866a88a9466f24d0f11d47b38d878b6d10

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -922,7 +922,7 @@ status_t ACodec::setupNativeWindowSizeFormatAndUsage(
             nativeWindow,
             def.format.video.nFrameWidth,
             def.format.video.nFrameHeight,
-            def.format.video.eColorFormat,
+            OMXCodec::OmxToHALFormat(def.format.video.eColorFormat),
             mRotationDegrees,
             usage);
 }
@@ -5255,7 +5255,7 @@ bool ACodec::BaseState::onOMXFillBufferDone(
                 mCodec->mSkipCutBuffer->submit(info->mData);
             }
             info->mData->meta()->setInt64("timeUs", timeUs);
-            info->mData->meta()->setObject("graphic-buffer", info->mGraphicBuffer);
+            //info->mData->meta()->setObject("graphic-buffer", (android::RefBase*)&info->mGraphicBuffer);
 
             sp<AMessage> notify = mCodec->mNotify->dup();
             notify->setInt32("what", CodecBase::kWhatDrainThisBuffer);
